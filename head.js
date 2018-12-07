01b4f0eae3b8039bb244f9382131b45185e01990
Keerthy E B <44020061+keerthyeb@users.noreply.github.com>
2018-12-07 14:17:57 +0530

    Modify head.js

--- head.js
+++ head.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
-const { head }= require("./src/library.js");
+const {head} = require('./src/library.js');
 
-const main = function(){
-  console.log(head(fs,process.argv.slice(2)));
-}
+const main = function() {
+  console.log(head(fs, process.argv.slice(2)));
+};
 
 main();
-
-
