7415a2788aa802b65c981785edafa333c130abb8
Keerthy E B <44020061+keerthyeb@users.noreply.github.com>
2018-12-17 10:23:54 +0530

    Modify test cases in handleInputTest.js

--- test/handleInputTest.js
+++ test/handleInputTest.js
@@ -1,32 +1,39 @@
-const { deepEqual } = require("assert");
+const assert = require("assert");
 const { getParameters, classifyInputs } = require("../src/handleInput.js");
 
 describe("getParameters", function() {
-  it("should return an object with option n and numberOfLines, file in files array ", () => {
-    deepEqual(getParameters(["-5", "file.txt"]), {
+  it("should return an object with option n,numberOfLines and file in files array ", () => {
+    let actualOutput = getParameters(["-5", "file.txt"]);
+    let expecyedOutput = {
       option: "n",
       numberOfLines: 5,
       files: ["file.txt"]
-    });
-    deepEqual(getParameters(["-10", "file.txt", "file2.txt", "file3.txt"]), {
-      option: "n",
-      numberOfLines: 10,
-      files: ["file.txt", "file2.txt", "file3.txt"]
-    });
+    };
+    assert.deepEqual(actualOutput, expecyedOutput);
+  });
+  it("should return return an object with option, count and files for multiple files", function() {
+    assert.deepEqual(
+      getParameters(["-10", "file.txt", "file2.txt", "file3.txt"]),
+      {
+        option: "n",
+        numberOfLines: 10,
+        files: ["file.txt", "file2.txt", "file3.txt"]
+      }
+    );
   });
 
   it("should return an object of option, numberOfLines and files ", function() {
-    deepEqual(getParameters(["-n1", "file.txt"]), {
+    assert.deepEqual(getParameters(["-n1", "file.txt"]), {
       option: "n",
       numberOfLines: 1,
       files: ["file.txt"]
     });
-    deepEqual(getParameters(["-n", "10", "file.txt"]), {
+    assert.deepEqual(getParameters(["-n", "10", "file.txt"]), {
       option: "n",
       numberOfLines: 10,
       files: ["file.txt"]
     });
-    deepEqual(getParameters(["-n", "-1", "file.txt", "file2.txt"]), {
+    assert.deepEqual(getParameters(["-n", "-1", "file.txt", "file2.txt"]), {
       option: "n",
       numberOfLines: -1,
       files: ["file.txt", "file2.txt"]
@@ -34,17 +41,17 @@ describe("getParameters", function() {
   });
 
   it("should return an object of option c and numberOfLines of given value for passing input", function() {
-    deepEqual(getParameters(["-c1", "file.txt", "file2.txt"]), {
+    assert.deepEqual(getParameters(["-c1", "file.txt", "file2.txt"]), {
       option: "c",
       numberOfLines: 1,
       files: ["file.txt", "file2.txt"]
     });
-    deepEqual(getParameters(["-c", "1", "file.txt"]), {
+    assert.deepEqual(getParameters(["-c", "1", "file.txt"]), {
       option: "c",
       numberOfLines: 1,
       files: ["file.txt"]
     });
-    deepEqual(getParameters(["-c", "1", "file.txt", "file2.txt"]), {
+    assert.deepEqual(getParameters(["-c", "1", "file.txt", "file2.txt"]), {
       option: "c",
       numberOfLines: 1,
       files: ["file.txt", "file2.txt"]
@@ -55,7 +62,7 @@ describe("getParameters", function() {
 describe("classifyInputs", function() {
   describe("passing only file names", function() {
     it("should return an object with numberOfLines as 10 and file name in files array while passing file name as input", function() {
-      deepEqual(classifyInputs(["file.txt"]), {
+      assert.deepEqual(classifyInputs(["file.txt"]), {
         option: "n",
         numberOfLines: 10,
         files: ["file.txt"]
@@ -63,7 +70,7 @@ describe("classifyInputs", function() {
     });
 
     it("should return an object with numberOfLines as 10 and file names in files array while passing file names as input", function() {
-      deepEqual(classifyInputs(["file.txt", "file2.txt"]), {
+      assert.deepEqual(classifyInputs(["file.txt", "file2.txt"]), {
         option: "n",
         numberOfLines: 10,
         files: ["file.txt", "file2.txt"]
@@ -72,30 +79,33 @@ describe("classifyInputs", function() {
   });
 
   it("should return an object with option n and numberOfLines, file in files array while passing the numberOfLines & file as input", () => {
-    deepEqual(classifyInputs(["-5", "file.txt"]), {
+    assert.deepEqual(classifyInputs(["-5", "file.txt"]), {
       option: "n",
       numberOfLines: 5,
       files: ["file.txt"]
     });
-    deepEqual(classifyInputs(["-10", "file.txt", "file2.txt", "file3.txt"]), {
-      option: "n",
-      numberOfLines: 10,
-      files: ["file.txt", "file2.txt", "file3.txt"]
-    });
+    assert.deepEqual(
+      classifyInputs(["-10", "file.txt", "file2.txt", "file3.txt"]),
+      {
+        option: "n",
+        numberOfLines: 10,
+        files: ["file.txt", "file2.txt", "file3.txt"]
+      }
+    );
   });
 
   it("should return an object of option, numberOfLines and files when all three arguments are passed", function() {
-    deepEqual(classifyInputs(["-n1", "file.txt"]), {
+    assert.deepEqual(classifyInputs(["-n1", "file.txt"]), {
       option: "n",
       numberOfLines: 1,
       files: ["file.txt"]
     });
-    deepEqual(classifyInputs(["-n", "10", "file.txt"]), {
+    assert.deepEqual(classifyInputs(["-n", "10", "file.txt"]), {
       option: "n",
       numberOfLines: 10,
       files: ["file.txt"]
     });
-    deepEqual(classifyInputs(["-n", "-1", "file.txt", "file2.txt"]), {
+    assert.deepEqual(classifyInputs(["-n", "-1", "file.txt", "file2.txt"]), {
       option: "n",
       numberOfLines: -1,
       files: ["file.txt", "file2.txt"]
@@ -103,17 +113,17 @@ describe("classifyInputs", function() {
   });
 
   it("should return an object of option c and numberOfLines of givrn balue for passing input", function() {
-    deepEqual(classifyInputs(["-c1", "file.txt", "file2.txt"]), {
+    assert.deepEqual(classifyInputs(["-c1", "file.txt", "file2.txt"]), {
       option: "c",
       numberOfLines: 1,
       files: ["file.txt", "file2.txt"]
     });
-    deepEqual(classifyInputs(["-c", "1", "file.txt"]), {
+    assert.deepEqual(classifyInputs(["-c", "1", "file.txt"]), {
       option: "c",
       numberOfLines: 1,
       files: ["file.txt"]
     });
-    deepEqual(classifyInputs(["-c", "1", "file.txt", "file2.txt"]), {
+    assert.deepEqual(classifyInputs(["-c", "1", "file.txt", "file2.txt"]), {
       option: "c",
       numberOfLines: 1,
       files: ["file.txt", "file2.txt"]
