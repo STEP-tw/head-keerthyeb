66429531783dcd82cc8f421217d92c3a0a7ab17a
Keerthy E B <44020061+keerthyeb@users.noreply.github.com>
2018-12-17 16:07:55 +0530

    Modify test cases in handleInputTest.js

--- test/handleInputTest.js
+++ test/handleInputTest.js
@@ -91,73 +91,110 @@ describe("getParameters", function() {
 });
 
 describe("classifyInputs", function() {
-  describe("passing only file names", function() {
-    it("should return an object with numberOfLines as 10 and file name in files array while passing file name as input", function() {
-      assert.deepEqual(classifyInputs(["file.txt"]), {
+  describe("should return an object which includes option, numberOfLines and file names", function() {
+    it("should return the object if only file name is given", function() {
+      let actualOutput = classifyInputs(["file.txt"]);
+      let expectedOutput = {
         option: "n",
         numberOfLines: 10,
         files: ["file.txt"]
-      });
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
 
-    it("should return an object with numberOfLines as 10 and file names in files array while passing file names as input", function() {
-      assert.deepEqual(classifyInputs(["file.txt", "file2.txt"]), {
+    it("should return the object if only file names are given", function() {
+      let actualOutput = classifyInputs(["file.txt", "file2.txt"]);
+      let expectedOutput = {
         option: "n",
         numberOfLines: 10,
         files: ["file.txt", "file2.txt"]
-      });
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
-  });
 
-  it("should return an object with option n and numberOfLines, file in files array while passing the numberOfLines & file as input", () => {
-    assert.deepEqual(classifyInputs(["-5", "file.txt"]), {
-      option: "n",
-      numberOfLines: 5,
-      files: ["file.txt"]
+    it("should return the object if only count and file name are given", () => {
+      let actualOutput = classifyInputs(["-5", "file.txt"]);
+      let expectedOutput = {
+        option: "n",
+        numberOfLines: 5,
+        files: ["file.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
-    assert.deepEqual(
-      classifyInputs(["-10", "file.txt", "file2.txt", "file3.txt"]),
-      {
+
+    it("should return the object if only count and file names are given", function() {
+      let actualOutput = classifyInputs([
+        "-10",
+        "file.txt",
+        "file2.txt",
+        "file3.txt"
+      ]);
+      let expectedOutput = {
         option: "n",
         numberOfLines: 10,
         files: ["file.txt", "file2.txt", "file3.txt"]
-      }
-    );
-  });
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
+    });
 
-  it("should return an object of option, numberOfLines and files when all three arguments are passed", function() {
-    assert.deepEqual(classifyInputs(["-n1", "file.txt"]), {
-      option: "n",
-      numberOfLines: 1,
-      files: ["file.txt"]
+    it("should return the object when option and count are given together", function() {
+      let actualOutput = classifyInputs(["-n1", "file.txt"]);
+      let expectedOutput = {
+        option: "n",
+        numberOfLines: 1,
+        files: ["file.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
-    assert.deepEqual(classifyInputs(["-n", "10", "file.txt"]), {
-      option: "n",
-      numberOfLines: 10,
-      files: ["file.txt"]
-    });
-    assert.deepEqual(classifyInputs(["-n", "-1", "file.txt", "file2.txt"]), {
-      option: "n",
-      numberOfLines: -1,
-      files: ["file.txt", "file2.txt"]
-    });
-  });
 
-  it("should return an object of option c and numberOfLines of givrn balue for passing input", function() {
-    assert.deepEqual(classifyInputs(["-c1", "file.txt", "file2.txt"]), {
-      option: "c",
-      numberOfLines: 1,
-      files: ["file.txt", "file2.txt"]
+    it("should return the object when option and count are given separately", function() {
+      let actualOutput = classifyInputs(["-n", "10", "file.txt"]);
+      let expectedOutput = {
+        option: "n",
+        numberOfLines: 10,
+        files: ["file.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
-    assert.deepEqual(classifyInputs(["-c", "1", "file.txt"]), {
-      option: "c",
-      numberOfLines: 1,
-      files: ["file.txt"]
+
+    it("should return the object when the count is negative", function() {
+      let actualOutput = classifyInputs(["-n", "-1", "file.txt", "file2.txt"]);
+      let expectedOutput = {
+        option: "n",
+        numberOfLines: -1,
+        files: ["file.txt", "file2.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
-    assert.deepEqual(classifyInputs(["-c", "1", "file.txt", "file2.txt"]), {
-      option: "c",
-      numberOfLines: 1,
-      files: ["file.txt", "file2.txt"]
+
+    it("should return the object if option 'c' and count are given together", function() {
+      let actualOutput = classifyInputs(["-c1", "file.txt", "file2.txt"]);
+      let expectedOutput = {
+        option: "c",
+        numberOfLines: 1,
+        files: ["file.txt", "file2.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
+    });
+
+    it("should return the object if option 'c' and count are given separately", function() {
+      let actualOutput = classifyInputs(["-c", "1", "file.txt"]);
+      let expectedOutput = {
+        option: "c",
+        numberOfLines: 1,
+        files: ["file.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
+    });
+
+    it("should return the object if option 'c' and count are given with multiple files", function() {
+      let actualOutput = classifyInputs(["-c", "1", "file.txt", "file2.txt"]);
+      let expectedOutput = {
+        option: "c",
+        numberOfLines: 1,
+        files: ["file.txt", "file2.txt"]
+      };
+      assert.deepEqual(actualOutput, expectedOutput);
     });
   });
 });
