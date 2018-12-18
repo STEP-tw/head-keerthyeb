d64644d2dbf4c3c30dd71fc627d2b04c33b747e9
Keerthy E B <44020061+keerthyeb@users.noreply.github.com>
2018-12-18 10:17:34 +0530

    Avoid duplicates test cases in string.js

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 set -e
 ./scripts/run_test.sh head.js ./app_tests_data/headInput ./app_tests_data/headOutput 
-mocha --reporter min
+mocha --recursive
--- test/util/stringTest.js
+++ test/util/stringTest.js
@@ -8,58 +8,45 @@ const {
 
 describe("Test for getFirstNCharacters", function() {
   it("should return nothing for an empty string", function() {
-    let empty = "";
-    assert.deepEqual(getFirstNCharacters(empty, 0), "");
-    assert.deepEqual(getFirstNCharacters(empty, 1), "");
+    assert.deepEqual(getFirstNCharacters("", 1), "");
   });
 
   it("should return given number of bytes from a given text", function() {
     let numbers = "1\n2\n3";
-    assert.deepEqual(getFirstNCharacters(numbers, 0), "");
     assert.deepEqual(getFirstNCharacters(numbers, 2), "1\n");
   });
 });
 
 describe("Test for getHeadLines", function() {
   it("should return nothing for an empty string", function() {
-    let empty = "";
-    assert.deepEqual(getHeadLines(empty, 0), "");
-    assert.deepEqual(getHeadLines(empty, 1), "");
+    assert.deepEqual(getHeadLines("", 1), "");
   });
 
   it("should return given number of lines from a given text", function() {
     let numbers = "1\n2\n3";
-    assert.deepEqual(getHeadLines(numbers, 0), "");
     assert.deepEqual(getHeadLines(numbers, 2), "1\n2");
-    assert.deepEqual(getHeadLines(numbers, 4), "1\n2\n3");
   });
 });
 
 describe("Test for getTailLines", function() {
   it("should return nothing for an empty string", function() {
     let empty = "";
-    assert.deepEqual(getTailLines(empty, 0), "");
-    assert.deepEqual(getTailLines(empty, 1), "");
+    assert.deepEqual(getTailLines("", 1), "");
   });
 
   it("should return given number of last lines from a given text", function() {
     let numbers = "1\n2\n3";
-    assert.deepEqual(getTailLines(numbers, 0), "");
     assert.deepEqual(getTailLines(numbers, 2), "2\n3");
-    assert.deepEqual(getTailLines(numbers, 4), "1\n2\n3");
   });
 });
 
 describe("Test for getLastNCharacters", function() {
   it("should return nothing for an empty string", function() {
-    let empty = "";
-    assert.deepEqual(getLastNCharacters(empty, 0), "");
-    assert.deepEqual(getLastNCharacters(empty, 1), "");
+    assert.deepEqual(getLastNCharacters("", 1), "");
   });
 
   it("should return given number of last bytes from a given text", function() {
     let numbers = "1\n2\n3";
-    assert.deepEqual(getLastNCharacters(numbers, 0), "");
     assert.deepEqual(getLastNCharacters(numbers, 2), "\n3");
   });
 });
